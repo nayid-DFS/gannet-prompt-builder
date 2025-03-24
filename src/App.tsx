501b192ae7b32ca13e9c96dda2fc2622602b126a
nayid-dfs <nayid@datafriendlyspace.org>
2025-03-24 14:39:32 -0500

    Fixed logo and added a new logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
 import PromptBuilderForm from './components/PromptBuilderForm';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="gannet-app">
       <header className="gannet-header">
         <div className="container">
-          <h1 className="gannet-title">GANNET Prompt Builder</h1>
+          <div className="header-content">
+            <div className="logo-container">
+              <img src={logo} alt="GANNET Logo" className="gannet-logo" />
+            </div>
+            <h1 className="gannet-title">GANNET Prompt Builder</h1>
+          </div>
         </div>
       </header>
       
